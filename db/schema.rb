995d88ad1608e44a485a2059173df3897763d6d3
Tsuji <geekjumpingcarnival@i.softbank.jp>
2024-09-03 11:47:12 +0900

    add author and picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_03_015317) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_03_023411) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.string "picture"
   end
 end
